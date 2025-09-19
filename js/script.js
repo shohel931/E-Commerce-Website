5a06767b7752a940864f34d7fbaba9ac24bf67ab
MD. Shohel Rana <shohel065931@gmail.com>
2025-09-19 18:09:01 +0600

    g

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ const mobileMenu = document.querySelector(".mobile-menu");
 hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
 });
+
+
+// Slider functionality
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+document.querySelector(".next").addEventListener("click", () => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+});
