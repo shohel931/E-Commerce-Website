cb62f6e2ae43c5c2240a0fd6f9f71e553f37ac48
MD. Shohel Rana <shohel065931@gmail.com>
2025-09-20 09:03:09 +0600

    g

--- js/script.js
+++ js/script.js
@@ -11,20 +11,23 @@ const slides = document.querySelectorAll('.slide');
 let index = 0;
 let slideInterval;
 
-document.querySelector(".next").addEventListener("click", () => {
-    index = (index + 1) % slides.length;
-    showSlide();
-});
-
-document.querySelector(".prev").addEventListener("click", () => {
-    index = (index - 1 + slides.length) % slides.length;
-    showSlide();
-});
 
 function showSlide() {
     const slidesContainer = document.querySelector('.slides');
     slidesContainer.style.transform = `translateX(-${index * 100}%)`;
 }
+
+
+function nextSlide() {
+    index = (index + 1) % slides.length;
+    showSlide();
+}
+
+function prevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide();
+}
+
 
 document.querySelector(".next").addEventListener("click", () => {
     nextSlide();
