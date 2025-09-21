c484b44bade40dd1f02eceda2c1a5206cf72f7af
MD. Shohel Rana <shohel065931@gmail.com>
2025-09-21 09:06:25 +0600

    y

--- js/script.js
+++ js/script.js
@@ -48,3 +48,9 @@ function resetAutoSlide() {
     startAutoSlide();
 }
 startAutoSlide();
+
+// Login / Register 
+function toggleForm(form) {
+    document.getElementById('loginForm').style.display = (form === "login") ? "block" : "none";
+    document.getElementById('registerForm').style.display = (form === "register") ? "block" : "none";
+}
